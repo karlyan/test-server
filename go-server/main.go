34028627ad48501ccc4c9312c10459f0d870e0ff
syan <yanshuhuan@gmail.com>
2021-05-23 16:57:59 -0500

    add remte ip

--- go-server/main.go
+++ go-server/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world")
+		ip, _ := c.RemoteIP()
+		c.String(200, fmt.Sprintf("request comes from: %v and RemoteIP is: %v\n", c.Request.RemoteAddr, ip))
 	})
+
 	r.Run("0.0.0.0:8080")
 }
