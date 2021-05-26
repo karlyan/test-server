8901fea6611fedf63c744e27186d151416961490
syan <yanshuhuan@gmail.com>
2021-05-25 22:12:24 -0500

    add debug output

--- go-server/main.go
+++ go-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http/httputil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,8 +10,16 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		ip, _ := c.RemoteIP()
-		c.String(200, fmt.Sprintf("request comes from: %v and RemoteIP is: %v\n", c.Request.RemoteAddr, ip))
+		c.String(200, fmt.Sprintf("request comes from: %v and ClientIP is: %v\n", c.Request.RemoteAddr, c.ClientIP()))
+	})
+
+	r.GET("/debug", func(c *gin.Context) {
+		requestDump, err := httputil.DumpRequest(c.Request, true)
+		if err != nil {
+			c.String(500, fmt.Sprintf("error: %v\n", err))
+			return
+		}
+		c.String(200, fmt.Sprintf("%s\n", requestDump))
 	})
 
 	r.Run("0.0.0.0:443")
